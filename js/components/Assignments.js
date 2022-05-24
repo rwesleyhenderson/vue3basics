7e5522973f004fa193a8118daf73caa3d27f35be
Wes Henderson <whenderson@nsgdv.com>
2022-05-23 21:39:59 -0500

    Ep13

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -14,11 +14,7 @@ export default {
 
     data() {
         return {
-            assignments: [
-                {name: 'Finish Project', complete: false, id: 1, tag: 'math'},
-                {name: 'Read chapter 4', complete: false, id: 2, tag: 'science'},
-                {name: 'Turn in homework', complete: false, id: 3, tag: 'math'},
-            ],
+            assignments: [],
         }
     },
 
@@ -29,6 +25,15 @@ export default {
                 completed: this.assignments.filter(assignment => assignment.complete)
             };
         }
+    },
+
+    created() {
+        //look into using axios here
+        fetch('http://localhost:3001/assignments')
+            .then(response => response.json())
+            .then(assingments => {
+                this.assignments = assingments;
+            });
     },
 
     methods: {
